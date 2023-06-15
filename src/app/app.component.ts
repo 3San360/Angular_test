b4bc7e837c57043ebdc4390408df32596564ac0e
3san360 <89893114+3San360@users.noreply.github.com>
2023-06-15 10:29:32 +0530

    Added NGX SPINNER

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { NgxSpinnerService } from "ngx-spinner";
+// import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  //   template: `
+  //   <h1>Hello World!</h1>
+  //   <p>This template definition spans multiple lines.</p>
+  // `,
   styleUrls: ['./app.component.scss']
+  // styles: ['h1 { font-weight: normal; }']
 })
 export class AppComponent {
   title = 'my-app';
+
+  constructor(private spinner: NgxSpinnerService) {
+
+  }
+
+  // ngOnInit() {
+
+  // }
+
+  showSpinner() {
+    this.spinner.show()
+
+    setTimeout(() => {
+      this.spinner.hide()
+
+    }, 2000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { NgxSpinnerModule } from 'ngx-spinner';
+import { NgxSpinnerModule } from "ngx-spinner";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxSpinnerModule,
+    BrowserAnimationsModule
   ],
+  // exports:[NgxSpinnerModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
